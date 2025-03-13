cd298d70078084a9fd55cfc1388322f7605dda05
Kritkhong <kritkhong@gmail.com>
2025-03-13 12:45:35 +0700

    fix javascript on order adjuster

--- staticfiles/shoplist/script.js
+++ staticfiles/shoplist/script.js
@@ -70,32 +70,94 @@ $(".buy-form").submit(function (e){
 // })
 
 
-$(".adjust-order").submit(function (e){
-        e.preventDefault();
-        const serializedData = $(this).serialize()
-        const formData = new FormData(this);
-        url = formData.get('url')
-        // for (const pair of formData.entries()) {
-        //         console.log(pair[0], pair[1]);
-        //       }
-        $.ajax({
-                type: 'POST',
-                url: url,
-                data: serializedData,
-                success: function (response) {
-                        const instance = JSON.parse(response['instance'])
-                        const product = instance[0]
-                        // console.log(product);
-                        bought_amount = product["fields"]["bought_amount"];
-                        order_amount = product["fields"]["order_amount"];
-                        $("#order_"+product['pk']).text(order_amount);
-                        $("#order_"+product['pk']).css("color","red");
-                        adjustBuyIndicator(product['pk'], bought_amount, order_amount)
+// $(".adjust-order").submit(function (e){
+//         e.preventDefault();
+//         const serializedData = $(this).serialize()
+//         const formData = new FormData(this);
+//         url = formData.get('url')
+//         for (const pair of formData.entries()) {
+//                 console.log(pair[0], pair[1]);
+//               }
+//         $.ajax({
+//                 type: 'POST',
+//                 url: url,
+//                 data: serializedData,
+//                 success: function (response) {
+//                         const instance = JSON.parse(response['instance'])
+//                         const product = instance[0]
+//                         // console.log(product);
+//                         bought_amount = product["fields"]["bought_amount"];
+//                         order_amount = product["fields"]["order_amount"];
+//                         $("#order_"+product['pk']).text(order_amount);
+//                         $("#order_"+product['pk']).css("color","red");
+//                         adjustBuyIndicator(product['pk'], bought_amount, order_amount)
 
-                },
-                error: function (response) {
-                        alert(response["responseJSON"]["error"]);
-                }
+//                 },
+//                 error: function (response) {
+//                         alert(response["responseJSON"]["error"]);
+//                 }
+
+//         })
+// })
+
+function getCookie(name) {
+        let cookieValue = null;
+        if (document.cookie && document.cookie !== "") {
+          const cookies = document.cookie.split(";");
+          for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + "=")) {
+              cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+              break;
+            }
+          }
+        }
+        return cookieValue;
+      }
+
+orderAdjBtns = document.getElementsByClassName("adjust-order-btn")
+for (let i = 0; i < orderAdjBtns.length; i++ ){
+        orderAdjBtns[i].addEventListener("click", function () {
+                const formData = new FormData();
+                // console.log(this);
+                const productId = this.getAttribute("product-id");
+                const value = this.getAttribute("value");
+                formData.append("product_id", productId);
+                formData.append("order_adjust", value);
+                const url = this.getAttribute("url");
+                const serializedData = new URLSearchParams(formData);
+                fetch(url, {
+                        method: "POST",
+                        credentials: "same-origin",
+                        headers: {
+                          "x-requested-with": "XMLHttpRequest",
+                          "X-CSRFToken": getCookie("csrftoken"),
+                        },
+                        body: formData
+                      })
+                      .then(response => response.json())
+                      .then(function(data) {
+                        if (data.hasOwnProperty("instance")){
+                                const instance = JSON.parse(data['instance']);
+                                const product = instance[0];
+                                console.log(product);    
+                                boughtAmount = product["fields"]["bought_amount"];
+                                orderAmount = product["fields"]["order_amount"];
+                                orderCountObj = document.getElementById("order_"+product['pk'])
+                                orderCountObj.textContent = orderAmount;
+                                orderCountObj.style.color = "red";
+                                adjustBuyIndicator(product['pk'], boughtAmount, orderAmount)
+
+                        } else {
+                                alert(data['error'])
+                        }
+                                
+
+
+                      })
+
 
         })
-})
+}
+
